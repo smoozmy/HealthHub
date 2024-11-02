a6426f4c55cdf1ffd999c42e8d13386c9f340a85
smoozmy <smoozmy@yandex.ru>
2024-11-02 11:32:06 +0300

    Initial Commit

---
+++ HealthHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthHub
+//
+//  Created by Александр Крапивин on 02.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
