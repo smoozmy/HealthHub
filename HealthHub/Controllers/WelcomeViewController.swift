4fddc92c7b9819cc317a17851b7fbfc09aa1c4bc
smoozmy <smoozmy@yandex.ru>
2024-11-03 14:33:29 +0300

    Поправлена верстка авторизации

---
+++ HealthHub/Controllers/WelcomeViewController.swift
@@ -0,0 +1,112 @@
+import UIKit
+
+final class WelcomeViewController: UIViewController {
+    
+    // MARK: - UI Components
+    
+    private lazy var bgView: UIView = {
+        let element = UIView()
+        element.backgroundColor = .white30
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Добро пожаловать!"
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+
+    
+    private lazy var loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Уже есть аккаунт", for: .normal)
+        button.backgroundColor = .systemIndigo
+        button.layer.cornerRadius = 15
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .semibold)
+        button.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var registerButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Зарегистрироваться", for: .normal)
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 15
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.systemIndigo.cgColor
+        button.setTitleColor(.systemIndigo, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .semibold)
+        button.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupView()
+        setupConstraints()
+        
+        let gradientLayer = Colors.angularGradientLayerSetupGoal(bounds: view.bounds)
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    private func setupView() {
+        view.addSubview(bgView)
+        view.addSubview(titleLabel)
+        view.addSubview(loginButton)
+        view.addSubview(registerButton)
+    }
+    
+    // MARK: - Actions
+    
+    @objc private func loginButtonTapped() {
+        print("WelcomeVC: Войти нажата")
+        let loginVC = LoginViewController()
+        loginVC.modalPresentationStyle = .fullScreen
+        present(loginVC, animated: true)
+    }
+    
+    @objc private func registerButtonTapped() {
+        print("WelcomeVC: Регистрация нажата")
+        let registerVC = RegistrationViewController()
+        registerVC.modalPresentationStyle = .fullScreen
+        present(registerVC, animated: true)
+    }
+    
+    // MARK: - Constraints
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            
+            bgView.topAnchor.constraint(equalTo: view.topAnchor),
+            bgView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            bgView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            bgView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+
+            
+            loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            loginButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            registerButton.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 20),
+            registerButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            registerButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            registerButton.heightAnchor.constraint(equalToConstant: 50),
+        ])
+    }
+}
